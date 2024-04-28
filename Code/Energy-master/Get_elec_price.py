89a942da0eee4634256b98bfd14cee4d41cdf0e6
Peilin Liu <142320837+u7518297@users.noreply.github.com>
2024-04-28 10:36:39 +0800

    Update Get_elec_price.py

--- Code/Energy-master/Get_elec_price.py
+++ Code/Energy-master/Get_elec_price.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 # Get electricity prices in different countries
+# Note: The data of the prices are offered by electricity_price.csv, which includes the data in 2019 with 188 countries. 
 def get_elec_price(country_code):
     data = pd.read_csv('electricity_price.csv', sep=',')
     try:
